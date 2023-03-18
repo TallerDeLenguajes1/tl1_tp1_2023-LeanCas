aa669b6ea2c917374de12658316dc73e482a82e8
Leandro Casavalle <leandrocasavalle10@gmail.com>
2023-03-18 16:56:16 -0300

    Segundo commit del archivo C

--- Problema_3/tp1_1.c
+++ Problema_3/tp1_1.c
@@ -2,7 +2,23 @@
 
 int main() {
 
-   printf("Hola mundo ");
-   
-   return 0;
+    int *punt, num;
+
+    num = 10;
+
+    punt = &num;
+
+    printf("1)- El contenido del puntero : %d ",*punt);
+
+    printf("\n2)- La direccion de memoria almacenada por el puntero : %d ",punt);
+
+    printf("\n3)- La direccion de memoria de la variable : %d ",&num);
+
+    printf("\n4)- La direccion de memoria del puntero : %d ",&punt);
+
+    int valor = sizeof(num);
+
+    printf("\n5)- El tamaño de memoria utilizado por esa variable usando la funcion sizeof() : %d ",valor);
+
+    return 0;
 }
